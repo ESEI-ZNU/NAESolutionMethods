c3a721d71da01196fc2229336c6674ea8e39110c
Kyragga <topbanertop@gmail.com>
2026-04-05 17:11:00 +0300

    implemented program skeleton for root finding + comparing methods

--- src/mypackage/Main.java
+++ src/mypackage/Main.java
@@ -1,105 +1,8 @@
 package mypackage;
 
-//public class Main {
-//    public static void main(String[] args) {
-//        System.out.println("Hello, World!");
-//    }
-//}
-
 import java.util.Scanner;
 
 public class Main {
-
-    static double bruteForce(double min, double max, double step, double epsilon, double[] k) {
-
-        double root = 0;
-
-        long startTime = System.nanoTime();
-
-        /// METHOD
-
-        long endTime = System.nanoTime();
-
-        long diffTime = endTime - startTime;
-
-        System.out.println("Error = " + epsilon);
-        System.out.println("Time = " + diffTime + " ns");
-
-        return root;
-    }
-
-    static double bisection(double min, double max, double epsilon, double[] k) {
-
-        double root = 0;
-
-        long startTime = System.nanoTime();
-
-        /// METHOD
-
-        long endTime = System.nanoTime();
-
-        long diffTime = endTime - startTime;
-
-        System.out.println("Error = " + epsilon);
-        System.out.println("Time = " + diffTime + " ns");
-
-        return root;
-    }
-
-    static double chord(double min, double max, double epsilon, double[] k) {
-
-        double root = 0;
-
-        long startTime = System.nanoTime();
-
-        /// METHOD
-
-        long endTime = System.nanoTime();
-
-        long diffTime = endTime - startTime;
-
-        System.out.println("Error = " + epsilon);
-        System.out.println("Time = " + diffTime + " ns");
-
-        return root;
-    }
-
-    static double newton(double start, double epsilon, double[] k) {
-
-        double root = 0;
-
-        long startTime = System.nanoTime();
-
-        /// METHOD
-
-        long endTime = System.nanoTime();
-
-        long diffTime = endTime - startTime;
-
-        System.out.println("Error = " + epsilon);
-        System.out.println("Time = " + diffTime + " ns");
-
-        return root;
-    }
-
-    static double iteration(double start, double epsilon, double[] k) {
-
-        double root = 0;
-
-        long startTime = System.nanoTime();
-
-        /// METHOD
-
-        long endTime = System.nanoTime();
-
-        long diffTime = endTime - startTime;
-
-        System.out.println("Error = " + epsilon);
-        System.out.println("Time = " + diffTime + " ns");
-
-        return root;
-    }
-
 
     public static void main(String[] args) {
 
@@ -153,7 +56,7 @@ public class Main {
 
                     System.out.print("1. Brute force method (переборка)");
 
-                    root1 = bruteForce(min, max, step, epsilon, ArrayOfK);
+                    root1 = Function.bruteForce(min, max, step, epsilon, ArrayOfK, count);
 
                     System.out.println("Root = " + root1);
 
@@ -167,5 +70,162 @@ public class Main {
 
             }
         }
+    }
+}
+
+public class Method  {
+
+        static double bruteForce(double a, double b, double step, double epsilon, double[] k, int count)
+        {
+            long startTime = System.nanoTime();
+
+            double root = 0;
+
+            epsilon=step;
+
+            double x=a;
+
+            if(a==b)
+            {
+                if (Function.f(a, count, k ) == 0)
+                    return a;
+                else
+                    System.out.println("Немає коренів на проміжку");
+            }
+
+            while(x<b)
+            {
+                double nextX=0;
+                if ( Function.f(x, count, k )*Function.f(nextX, count, k )< 0)
+                {
+                    root = (x+nextX)/2;
+                    break;
+                }
+
+                else
+                    x=nextX;
+            }
+
+            long endTime = System.nanoTime();
+
+            long diffTime = endTime - startTime;
+
+            System.out.println("Error = " + epsilon);
+            System.out.println("Time = " + diffTime + " ns");
+
+            return root;
+        }
+
+    static double bisection(double a, double b, double epsilon, double[] k, int count) {
+
+        long startTime = System.nanoTime();
+
+        if (Function.f(a, count, k ) * Function.f(b, count, k ) > 0)
+            System.out.println("метод половинного ділення на цьому проміжку не можна застосувати");
+
+        double root = 0;
+        double medium = (a + b) / 2;
+
+
+        if (a == b) {
+            if (Function.f(a, count, k ) == 0)
+                return a;
+            else
+                System.out.println("Немає коренів на проміжку");
+        }
+
+        while ((b - a) > epsilon) {
+            if (Function.f(a, count, k ) * Function.f(medium, count, k ) < 0) {
+                b = medium;
+                medium = (a + b) / 2;
+                root = medium;
+            } else if (Function.f(b, count, k ) * Function.f(medium, count, k ) < 0) {
+                a = medium;
+                medium = (a + b) / 2;
+                root = medium;
+            }
+        }
+
+        long endTime = System.nanoTime();
+
+        long diffTime = endTime - startTime;
+
+        System.out.println("Error = " + epsilon);
+        System.out.println("Time = " + diffTime + " ns");
+
+        return root;
+
+    }
+
+        static double f_chord(double min, double max, double epsilon, double[] k) {
+
+            double root = 0;
+
+            long startTime = System.nanoTime();
+
+            /// METHOD
+
+            long endTime = System.nanoTime();
+
+            long diffTime = endTime - startTime;
+
+            System.out.println("Error = " + epsilon);
+            System.out.println("Time = " + diffTime + " ns");
+
+            return root;
+        }
+
+        static double f_newton(double start, double epsilon, double[] k) {
+
+            double root = 0;
+
+            long startTime = System.nanoTime();
+
+            /// METHOD
+
+            long endTime = System.nanoTime();
+
+            long diffTime = endTime - startTime;
+
+            System.out.println("Error = " + epsilon);
+            System.out.println("Time = " + diffTime + " ns");
+
+            return root;
+        }
+
+        static double f_iteration(double start, double epsilon, double[] k) {
+
+            double root = 0;
+
+            long startTime = System.nanoTime();
+
+            /// METHOD
+
+            long endTime = System.nanoTime();
+
+            long diffTime = endTime - startTime;
+
+            System.out.println("Error = " + epsilon);
+            System.out.println("Time = " + diffTime + " ns");
+
+            return root;
+        }
+
+}
+
+public class Function {
+
+    public static double f(double x, int count, double k[]) {
+
+        double polynom = 0;
+
+        for (int i = count; i > 0; i--) {
+
+            polynom = k[i] * Math.pow(x, i) + polynom;
+
+        }
+
+        return polynom;
+
     }
 }
